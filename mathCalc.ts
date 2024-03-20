b8923b3291d854c1185e86c931dee6ede739884c
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-03-20 16:50:13 +0100

    Update mathCalc.ts

--- mathCalc.ts
+++ mathCalc.ts
@@ -1,11 +1,91 @@
-type Layout_DESKTOP_IFC = {
+const LocalStorage_ALL: {
+    setPageOpenCalc_AEL: Function,
+    clcBt_EVT: EventTarget,
+    clcBt_DIV: HTMLDivElement,
+    clcBt_ID: number,
+    getLocalStorage: Function,
+    //localStorage_AR: [{ calculator: string, history: string }][]
+} = {
+    //localStorage_AR: [],
+    clcBt_EVT: null,
+    clcBt_DIV: null,
+    clcBt_ID: 0,
+    setPageOpenCalc_AEL(): void {
+        //this.localStorage_AR.push([{ calculator: "00", history: "none" }]);
+        const calcButton_AR: HTMLCollection = document.querySelector("div.nav-button-box").children;
+        const titleArea_DIV: HTMLDivElement = document.querySelector("div.cb-title");
+        for (let i: number = 0; i < calcButton_AR.length; i++){
+            calcButton_AR[i].addEventListener('click', (e) => {
+                this.clcBt_EVT = e.currentTarget;
+                this.clcBt_DIV = this.clcBt_EVT as HTMLDivElement;
+                this.clcBt_ID = Number(this.clcBt_DIV.id.slice(4, 6));
+                switch (this.clcBt_ID) {
+                    case 0:
+                        titleArea_DIV.textContent = "Kalkulator podstawowy";
+                        localStorage.setItem('calculator', '0');
+                        break;
+                    case 1:
+                        titleArea_DIV.textContent = "Kalkulator binarny";
+                        localStorage.setItem('calculator', '1');
+                        break;
+                    case 2:
+                        titleArea_DIV.textContent = "Logika matematyczna";
+                        localStorage.setItem('calculator', '2')
+                        break;
+                    case 3:
+                        titleArea_DIV.textContent = "NWD";
+                        localStorage.setItem('calculator', '3');
+                        break;
+                    case 4:
+                        titleArea_DIV.textContent = "NWW";
+                        localStorage.setItem('calculator', '4');
+                        break;
+                    case 5:
+                        titleArea_DIV.textContent = "Rozkład liczby na czynniki pierwsze";
+                        localStorage.setItem('calculator', '5');
+                        break;
+                }
+            }, false);
+        }
+    },
+    getLocalStorage(): void {
+        const titleArea_DIV: HTMLDivElement = document.querySelector("div.cb-title");
+        let calcType = localStorage.getItem('calculator');
+        (calcType === undefined || calcType === null) ? calcType = "0" : calcType;
+        if (calcType) {
+            switch (Number(calcType)) {
+                case 0:
+                    titleArea_DIV.textContent = "Kalkulator podstawowy";
+                    break;
+                case 1:
+                    titleArea_DIV.textContent = "Kalkulator binarny";
+                    break;
+                case 2:
+                    titleArea_DIV.textContent = "Logika matematyczna";
+                    break;
+                case 3:
+                    titleArea_DIV.textContent = "NWD";
+                    break;
+                case 4:
+                    titleArea_DIV.textContent = "NWW";
+                    break;
+                case 5:
+                    titleArea_DIV.textContent = "Rozkład liczby na czynniki pierwsze";
+                    break;
+            }
+        }
+    }
+}
+LocalStorage_ALL.getLocalStorage();
+LocalStorage_ALL.setPageOpenCalc_AEL();
+//console.log(LocalStorage_ALL.localStorage_AR);
+
+
+
+const Layout_DESKTOP: {
     setMenuButton_AEL: Function,
-    setContentCalendarBox_Width_AEL: Function,
-    setContentCalendarBox_Width_FNC: Function
-};
-class Layout_DESKTOP implements Layout_DESKTOP {
-    constructor() {}
-    public setMenuButton_AEL(): void {
+} = {
+    setMenuButton_AEL(): void {
         const menuButton_EL: HTMLDivElement = document.querySelector('svg.dsk-menu-arrow');
         let isShow: boolean = false;
         menuButton_EL.addEventListener('click', () => {
@@ -16,7 +96,7 @@ class Layout_DESKTOP implements Layout_DESKTOP {
                     setTimeout(() => {
                         sidebar.style.left = '0px';
                         menuButton_EL.style.transform = 'rotateZ(0deg)';
-                        sidebar.style.transitionDuration = '0.5s';
+                        sidebar.style.transitionDuration = '0.35s';
                     }, 1);
                 } break;
                 case true: {
@@ -24,27 +104,21 @@ class Layout_DESKTOP implements Layout_DESKTOP {
                     setTimeout(() => {
                         sidebar.style.left = '-270px';
                         menuButton_EL.style.transform = 'rotateZ(180deg)';
-                        sidebar.style.transitionDuration = '0.5s';
+                        sidebar.style.transitionDuration = '0.35s';
                     }, 1);
                 } break;
             }
         }, false);
     }
-};
+}
+Layout_DESKTOP.setMenuButton_AEL();
 
 
 
-type Layout_IFC = {
+const Layout_ALL: {
     setAppBody_Height_AEL: Function,
-    setLayout_DESKTOP: Function
-};
-// MEGA WAŻNE!
-// Chociaż klasa "Layout" może działać bez konstruktora, to w przypadku,
-// gdy jest ona używana wewnątrz innej klasy (takiej jak klasa App),
-// konstruktor jest potrzebny.
-class Layout implements Layout_IFC {
-    constructor() {}
-    public setAppBody_Height_AEL(): void {
+} = {
+    setAppBody_Height_AEL(): void {
         const appBody_EL: HTMLDivElement = document.querySelector('div.app-body');
         ['load', 'resize'].forEach((ev) => {
             addEventListener(ev, () => {
@@ -52,30 +126,5 @@ class Layout implements Layout_IFC {
             }, false);
         });
     }
-    setLayout_DESKTOP(): void {
-        const layout_DESKTOP: Layout_DESKTOP = new Layout_DESKTOP();
-        layout_DESKTOP.setMenuButton_AEL();
-        //layout_DESKTOP.setContentCalendarBox_Width_AEL();
-    }
-};
-
-
-
-// MEGA WAŻNE!
-// W JavaScript nie mogę programować obiektowo tak jak w c#, ponieważ deklaracje klas
-// wywołują się tak jak deklaracje zmiennych - proceduralnie. Kiedy klasa "App"
-// znajdowałaby się przed klasą "Layout", wówczas nie można byłoby zadeklarować instancji
-// klasy "Layout", ponieważ deklaracja tej klasy znajdowałaby się pod odwołaniem do niej,
-// a nie przed.
-type App_IFC = {
-    Main: Function
-};
-class App implements App_IFC {
-    public Main(): void {
-        const layout: Layout = new Layout();
-        layout.setAppBody_Height_AEL();
-        layout.setLayout_DESKTOP();
-    }
-};
-const app: App = new App();
-app.Main();
+}
+Layout_ALL.setAppBody_Height_AEL();
