149059ba9a5570c09b18856a3ad987511d7de63b
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-03-22 00:58:46 +0100

    Update mathCalc.js

--- mathCalc.js
+++ mathCalc.js
@@ -1,18 +1,76 @@
+var Layout_ALL = {
+    setAppBody_Height_AEL: function () {
+        var appBody_EL = document.querySelector('div.app-body');
+        ['load', 'resize'].forEach(function (ev) {
+            addEventListener(ev, function () {
+                appBody_EL.style.height = window.innerHeight + 'px';
+            }, false);
+        });
+    }
+};
+Layout_ALL.setAppBody_Height_AEL();
+var Layout_DESKTOP = {
+    setMenuButton_AEL: function () {
+        var menuButton_EL = document.querySelector('svg.dsk-menu-arrow');
+        var isShow = false;
+        menuButton_EL.addEventListener('click', function () {
+            var sidebar = document.querySelector('nav.dsk-nav-menu');
+            switch (isShow) {
+                case false:
+                    {
+                        isShow = true;
+                        setTimeout(function () {
+                            sidebar.style.left = '0px';
+                            menuButton_EL.style.transform = 'rotateZ(0deg)';
+                            sidebar.style.transitionDuration = '0.35s';
+                        }, 1);
+                    }
+                    break;
+                case true:
+                    {
+                        isShow = false;
+                        setTimeout(function () {
+                            sidebar.style.left = '-300px';
+                            menuButton_EL.style.transform = 'rotateZ(180deg)';
+                            sidebar.style.transitionDuration = '0.35s';
+                        }, 1);
+                    }
+                    break;
+            }
+        }, false);
+    },
+    setScrollBarPosition: function () {
+        window.addEventListener("DOMContentLoaded", function () {
+            var container = document.querySelectorAll('.ct-refactoring > .screen > .screen-position > .screen-hanger > .info, .ct-refactoring > .screen > .screen-position > .screen-hanger > .value');
+            for (var i = 0; i < container.length; i++) {
+                container[i].scrollLeft = container[i].scrollWidth;
+            }
+        }, false);
+    }
+};
+Layout_DESKTOP.setMenuButton_AEL();
+Layout_DESKTOP.setScrollBarPosition();
+// Błąd! Przy NWW nie można używać [,]
+// Błąd! Przy FAC nie można używać [,]
+// Błąd! Muszisz podać minimum 2 liczby   // NWD i NWW
+// [liczba] | NWD | Wynik:   // [liczba]
+// [liczba] | NWW | Wynik:   // [liczba]
+// [liczba] | FAC | Wynik:   // [liczby], ["liczba pierwsza"]
+// Błąd! Wprowadź poprawne dane
 var LocalStorage_ALL = {
     //localStorage_AR: [],
-    clcBt_EVT: null,
+    clcBt_EVT: undefined,
     clcBt_DIV: null,
     clcBt_ID: 0,
     clcType_EL: null,
     setPageOpenCalc_AEL: function () {
         var _this = this;
         //this.localStorage_AR.push([{ calculator: "00", history: "none" }]);
-        var calcButton_AR = document.querySelector('div.nav-button-box').children;
+        var calcButton_COLL = document.querySelector('div.nav-button-box').children;
         var titleArea_EL = document.querySelector('div.cb-title');
         var calcType_EL = document.querySelectorAll('div[class^="ct-"]');
-        console.log(calcType_EL);
-        for (var i = 0; i < calcButton_AR.length; i++) {
-            calcButton_AR[i].addEventListener('click', function (e) {
+        for (var i = 0; i < calcButton_COLL.length; i++) {
+            calcButton_COLL[i].addEventListener('click', function (e) {
                 _this.clcBt_EVT = e.currentTarget;
                 _this.clcBt_DIV = _this.clcBt_EVT;
                 _this.clcBt_ID = Number(_this.clcBt_DIV.id.slice(4, 6));
@@ -146,60 +204,135 @@ var LocalStorage_ALL = {
 LocalStorage_ALL.getLocalStorage();
 LocalStorage_ALL.setPageOpenCalc_AEL();
 //console.log(LocalStorage_ALL.localStorage_AR);
-var Layout_DESKTOP = {
-    setMenuButton_AEL: function () {
-        var menuButton_EL = document.querySelector('svg.dsk-menu-arrow');
-        var isShow = false;
-        menuButton_EL.addEventListener('click', function () {
-            var sidebar = document.querySelector('nav.dsk-nav-menu');
-            switch (isShow) {
-                case false:
-                    {
-                        isShow = true;
-                        setTimeout(function () {
-                            sidebar.style.left = '0px';
-                            menuButton_EL.style.transform = 'rotateZ(0deg)';
-                            sidebar.style.transitionDuration = '0.35s';
-                        }, 1);
-                    }
-                    break;
-                case true:
-                    {
-                        isShow = false;
-                        setTimeout(function () {
-                            sidebar.style.left = '-300px';
-                            menuButton_EL.style.transform = 'rotateZ(180deg)';
-                            sidebar.style.transitionDuration = '0.35s';
-                        }, 1);
-                    }
-                    break;
-            }
-        }, false);
-    }
-};
-Layout_DESKTOP.setMenuButton_AEL();
-var Layout_ALL = {
-    setAppBody_Height_AEL: function () {
-        var appBody_EL = document.querySelector('div.app-body');
-        ['load', 'resize'].forEach(function (ev) {
-            addEventListener(ev, function () {
-                appBody_EL.style.height = window.innerHeight + 'px';
+var Calculator_NWD_NWW_Faction_FUNCTIONS = {
+    bt_EVT: undefined,
+    bt_DIV: null,
+    bt_ID: "",
+    value: "0",
+    result: "",
+    FAC_isNotComma: true,
+    FAC_isDivided: false,
+    FAC_dividedNumber: 0,
+    FAC_factor: 2,
+    screen_INFO: document.querySelector('.ct-refactoring > .screen > .screen-position > .screen-hanger > .info'),
+    screen_VALUE: document.querySelector('.ct-refactoring > .screen > .screen-position > .screen-hanger > .value'),
+    setButtons_AEL: function () {
+        var _this = this;
+        var button_COLL = document.querySelector('.ct-refactoring > .buttons-group').children;
+        for (var i = 0; i < button_COLL.length; i++) {
+            button_COLL[i].addEventListener("click", function (e) {
+                _this.bt_EVT = e.currentTarget;
+                _this.bt_DIV = _this.bt_EVT;
+                _this.bt_ID = _this.bt_DIV.id;
+                //console.log(this.bt_ID);
+                switch (_this.bt_ID) {
+                    case "NWD":
+                        _this.operation_NWD();
+                        break;
+                    case "NWW":
+                        _this.operation_NWW();
+                        break;
+                    case "FAC":
+                        _this.operation_FAC();
+                        break;
+                    case "DEL":
+                        _this.operation_DEL(); // OK
+                        break;
+                    case "AC":
+                        _this.operation_AC(); // OK
+                        break;
+                    case ",":
+                        _this.operation_Comma(); // OK
+                        break;
+                    default:
+                        _this.operation_Number(_this.bt_ID); // OK
+                        break;
+                }
             }, false);
-        });
-    }
+        }
+    },
+    operation_NWD: function () {
+        //console.log(mode);
+    },
+    operation_NWW: function () {
+        //console.log(mode);
+    },
+    operation_FAC: function () {
+        // Jeżeli w wyrazie występuje znak [,], wyświetl błąd i zablokuj dalszą część operacji:
+        for (var i = 0; i < this.value.length; i++) {
+            if (this.value[i] === ",") {
+                this.FAC_isNotComma = false;
+            }
+            else {
+                this.FAC_isNotComma = true;
+            }
+        }
+        // Rozkład liczby całkowitej na czynniki pierwsze
+        if (this.FAC_isNotComma === true) {
+            this.result = "";
+            this.FAC_dividedNumber = Number(this.value);
+            while (this.FAC_dividedNumber > 1) {
+                this.FAC_factor = 2;
+                this.FAC_isDivided = false;
+                while (this.FAC_isDivided === false) {
+                    if (this.FAC_dividedNumber % this.FAC_factor === 0) {
+                        this.FAC_isDivided = true;
+                        this.result += String(this.FAC_factor) + ", ";
+                        this.FAC_dividedNumber = this.FAC_dividedNumber / this.FAC_factor;
+                        //alert("Czynniki: " + this.result + " | Pozostałość: " + this.FAC_dividedNumber + "| i: " + this.FAC_factor);
+                    }
+                    else if (this.FAC_dividedNumber % this.FAC_factor !== 0) {
+                        this.FAC_factor++;
+                    }
+                }
+            }
+            this.screen_INFO.textContent = "FAC | " + this.value + " | Wynik:";
+            this.FAC_isNotComma = true;
+            for (var i = 0; i < this.result.length; i++) {
+                if (this.result[i] === ",") {
+                    this.FAC_isNotComma = false;
+                }
+            }
+            // Ogarnij wyznaczanie liczby pierwszej!
+            if (this.FAC_isNotComma === false) {
+                this.result = this.result.slice(0, this.result.length - 2);
+            }
+            else if (this.FAC_isNotComma === true) {
+                this.result = "Liczba pierwsza";
+            }
+            this.screen_VALUE.textContent = this.result;
+        }
+    },
+    operation_DEL: function () {
+        // Jeżeli ostatni indeks ma wartość " " (" " za znakiem [,]), skasuj dwa miejsca, w przeciwnym razie 1:
+        if (this.value[this.value.length - 1] === " ") {
+            this.value = this.screen_VALUE.textContent.slice(0, (this.screen_VALUE.textContent.length - 2));
+        }
+        else {
+            this.value = this.screen_VALUE.textContent.slice(0, (this.screen_VALUE.textContent.length - 1));
+        }
+        if (this.value.length === 0) {
+            this.value = "0";
+        }
+        this.screen_VALUE.textContent = this.value;
+    },
+    operation_AC: function () {
+        this.value = "0";
+        this.screen_VALUE.textContent = this.value;
+    },
+    operation_Comma: function () {
+        this.value += ", ";
+        this.screen_VALUE.textContent = this.value;
+    },
+    operation_Number: function (numKey) {
+        // Jeżeli długośc wyrazu wynosi 1 i ma on wartośc 0, skasuj 0, a wstaw cyfrę, w przeciwnym razie dodaj cyfrę do wyrażenia:
+        if (this.value.length === 1 && this.value === "0") {
+            this.value = numKey;
+        }
+        else if (this.value.length > 1 || this.value !== "0") {
+            this.value += numKey;
+        }
+        this.screen_VALUE.textContent = this.value;
+    },
 };
-Layout_ALL.setAppBody_Height_AEL();
-// Błąd! Przy NWW nie można używać [,]
-// Błąd! Przy FAC nie można używać [,]
-// Błąd! Muszisz podać minimum 2 liczby   // NWD i NWW
-// [liczba] | NWD | Wynik:   // [liczba]
-// [liczba] | NWW | Wynik:   // [liczba]
-// [liczba] | FAC | Wynik:   // [liczby], ["liczba pierwsza"]
-// Błąd! Wprowadź poprawne dane
-// Ustawienie pozycji scrollbaru na właściwym miejscu (ekran kalkulatora)
-window.addEventListener("load", function () {
-    var container = document.querySelectorAll('.ct-refactoring > .screen > .screen-position > .screen-hanger > .info, .ct-refactoring > .screen > .screen-position > .screen-hanger > .value');
-    for (var i = 0; i < container.length; i++) {
-        container[i].scrollLeft = container[i].scrollWidth;
-    }
-}, false);
+Calculator_NWD_NWW_Faction_FUNCTIONS.setButtons_AEL();
