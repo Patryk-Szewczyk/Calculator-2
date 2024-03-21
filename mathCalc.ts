6143d98bee7f4176f4136a2893bae7b92bb566aa
Patryk Szewczyk <127959525+Patryk-Szewczyk@users.noreply.github.com>
2024-03-22 00:59:01 +0100

    Update mathCalc.ts

--- mathCalc.ts
+++ mathCalc.ts
@@ -1,3 +1,72 @@
+const Layout_ALL: {
+    setAppBody_Height_AEL: Function,
+} = {
+    setAppBody_Height_AEL(): void {
+        const appBody_EL: HTMLDivElement = document.querySelector('div.app-body');
+        ['load', 'resize'].forEach((ev) => {
+            addEventListener(ev, () => {
+                appBody_EL.style.height = window.innerHeight + 'px';
+            }, false);
+        });
+    }
+}
+Layout_ALL.setAppBody_Height_AEL();
+
+
+
+const Layout_DESKTOP: {
+    setMenuButton_AEL: Function,
+    setScrollBarPosition: Function  // Ustawienie pozycji scrollbaru na właściwym miejscu (ekran kalkulatora)
+} = {
+    setMenuButton_AEL(): void {
+        const menuButton_EL: HTMLDivElement = document.querySelector('svg.dsk-menu-arrow');
+        let isShow: boolean = false;
+        menuButton_EL.addEventListener('click', () => {
+            const sidebar: HTMLElement = document.querySelector('nav.dsk-nav-menu');
+            switch (isShow){
+                case false: {
+                    isShow = true;
+                    setTimeout(() => {
+                        sidebar.style.left = '0px';
+                        menuButton_EL.style.transform = 'rotateZ(0deg)';
+                        sidebar.style.transitionDuration = '0.35s';
+                    }, 1);
+                } break;
+                case true: {
+                    isShow = false;
+                    setTimeout(() => {
+                        sidebar.style.left = '-300px';
+                        menuButton_EL.style.transform = 'rotateZ(180deg)';
+                        sidebar.style.transitionDuration = '0.35s';
+                    }, 1);
+                } break;
+            }
+        }, false);
+    },
+    setScrollBarPosition() {
+        window.addEventListener("DOMContentLoaded", () => {
+            const container: NodeListOf<Element> = document.querySelectorAll('.ct-refactoring > .screen > .screen-position > .screen-hanger > .info, .ct-refactoring > .screen > .screen-position > .screen-hanger > .value');
+            for (let i: number = 0; i < container.length; i++) {
+                container[i].scrollLeft = container[i].scrollWidth;
+            }
+        }, false);
+    }
+}
+Layout_DESKTOP.setMenuButton_AEL();
+Layout_DESKTOP.setScrollBarPosition();
+
+
+
+// Błąd! Przy NWW nie można używać [,]
+// Błąd! Przy FAC nie można używać [,]
+// Błąd! Muszisz podać minimum 2 liczby   // NWD i NWW
+// [liczba] | NWD | Wynik:   // [liczba]
+// [liczba] | NWW | Wynik:   // [liczba]
+// [liczba] | FAC | Wynik:   // [liczby], ["liczba pierwsza"]
+// Błąd! Wprowadź poprawne dane
+
+
+
 const LocalStorage_ALL: {
     setPageOpenCalc_AEL: Function,
     clcBt_EVT: EventTarget,
@@ -8,18 +77,17 @@ const LocalStorage_ALL: {
     //localStorage_AR: [{ calculator: string, history: string }][]
 } = {
     //localStorage_AR: [],
-    clcBt_EVT: null,
+    clcBt_EVT: undefined,
     clcBt_DIV: null,
     clcBt_ID: 0,
     clcType_EL: null,
     setPageOpenCalc_AEL(): void {
         //this.localStorage_AR.push([{ calculator: "00", history: "none" }]);
-        const calcButton_AR: HTMLCollection = document.querySelector('div.nav-button-box').children;
+        const calcButton_COLL: HTMLCollection = document.querySelector('div.nav-button-box').children;
         const titleArea_EL: HTMLDivElement = document.querySelector('div.cb-title');
         const calcType_EL: NodeList = document.querySelectorAll('div[class^="ct-"]');
-        console.log(calcType_EL);
-        for (let i: number = 0; i < calcButton_AR.length; i++){
-            calcButton_AR[i].addEventListener('click', (e) => {
+        for (let i: number = 0; i < calcButton_COLL.length; i++){
+            calcButton_COLL[i].addEventListener('click', (e) => {
                 this.clcBt_EVT = e.currentTarget;
                 this.clcBt_DIV = this.clcBt_EVT as HTMLDivElement;
                 this.clcBt_ID = Number(this.clcBt_DIV.id.slice(4, 6));
@@ -148,68 +216,150 @@ LocalStorage_ALL.setPageOpenCalc_AEL();
 
 
 
-const Layout_DESKTOP: {
-    setMenuButton_AEL: Function,
+const Calculator_NWD_NWW_Faction_FUNCTIONS: {
+    setButtons_AEL: Function
+    bt_EVT: EventTarget,
+    bt_DIV: HTMLDivElement,
+    bt_ID: string,
+    value: string,
+    result: string,
+    screen_INFO: HTMLDivElement,
+    screen_VALUE: HTMLDivElement,
+    operation_NWD: Function,
+    operation_NWW: Function,
+    operation_FAC: Function,
+    FAC_isNotComma: boolean,
+    FAC_isDivided: boolean,
+    FAC_dividedNumber: number,
+    FAC_factor: number,
+    operation_DEL: Function,
+    operation_AC: Function,
+    operation_Comma: Function,
+    operation_Number: Function
 } = {
-    setMenuButton_AEL(): void {
-        const menuButton_EL: HTMLDivElement = document.querySelector('svg.dsk-menu-arrow');
-        let isShow: boolean = false;
-        menuButton_EL.addEventListener('click', () => {
-            const sidebar: HTMLElement = document.querySelector('nav.dsk-nav-menu');
-            switch (isShow){
-                case false: {
-                    isShow = true;
-                    setTimeout(() => {
-                        sidebar.style.left = '0px';
-                        menuButton_EL.style.transform = 'rotateZ(0deg)';
-                        sidebar.style.transitionDuration = '0.35s';
-                    }, 1);
-                } break;
-                case true: {
-                    isShow = false;
-                    setTimeout(() => {
-                        sidebar.style.left = '-300px';
-                        menuButton_EL.style.transform = 'rotateZ(180deg)';
-                        sidebar.style.transitionDuration = '0.35s';
-                    }, 1);
-                } break;
-            }
-        }, false);
-    }
-}
-Layout_DESKTOP.setMenuButton_AEL();
-
-
-
-const Layout_ALL: {
-    setAppBody_Height_AEL: Function,
-} = {
-    setAppBody_Height_AEL(): void {
-        const appBody_EL: HTMLDivElement = document.querySelector('div.app-body');
-        ['load', 'resize'].forEach((ev) => {
-            addEventListener(ev, () => {
-                appBody_EL.style.height = window.innerHeight + 'px';
+    bt_EVT: undefined,
+    bt_DIV: null,
+    bt_ID: "",
+    value: "0",
+    result: "",
+    FAC_isNotComma: true,
+    FAC_isDivided: false,
+    FAC_dividedNumber: 0,
+    FAC_factor: 2,
+    screen_INFO:  document.querySelector('.ct-refactoring > .screen > .screen-position > .screen-hanger > .info'),
+    screen_VALUE: document.querySelector('.ct-refactoring > .screen > .screen-position > .screen-hanger > .value'),
+    setButtons_AEL(): void {
+        const button_COLL: HTMLCollection = document.querySelector('.ct-refactoring > .buttons-group').children;
+        for (let i: number = 0; i < button_COLL.length; i++) {
+            button_COLL[i].addEventListener("click", (e) => {
+                this.bt_EVT = e.currentTarget;
+                this.bt_DIV = this.bt_EVT as HTMLDivElement;
+                this.bt_ID = this.bt_DIV.id;
+                //console.log(this.bt_ID);
+                switch(this.bt_ID) {
+                    case "NWD":
+                        this.operation_NWD();
+                        break;
+                    case "NWW":
+                        this.operation_NWW();
+                        break;
+                    case "FAC":
+                        this.operation_FAC();
+                        break;
+                    case "DEL":
+                        this.operation_DEL();  // OK
+                        break;
+                    case "AC":
+                        this.operation_AC();  // OK
+                        break;
+                    case ",":
+                        this.operation_Comma();  // OK
+                        break;
+                    default:
+                        this.operation_Number(this.bt_ID);  // OK
+                        break;
+                }
             }, false);
-        });
-    }
+        }
+    },
+    operation_NWD(): void {
+        //console.log(mode);
+    },
+    operation_NWW(): void {
+        //console.log(mode);
+    },
+    operation_FAC(): void {
+        // Jeżeli w wyrazie występuje znak [,], wyświetl błąd i zablokuj dalszą część operacji:
+        for (let i: number = 0; i < this.value.length; i++) {
+            if (this.value[i] === ",") {
+                this.FAC_isNotComma = false;
+            } else {
+                this.FAC_isNotComma = true;
+            }
+        }
+        // Rozkład liczby całkowitej na czynniki pierwsze
+        if (this.FAC_isNotComma === true) {
+            this.result = "";
+            this.FAC_dividedNumber = Number(this.value);
+            while (this.FAC_dividedNumber > 1) {
+                this.FAC_factor = 2;
+                this.FAC_isDivided = false;
+                while (this.FAC_isDivided === false) {
+                    if (this.FAC_dividedNumber % this.FAC_factor === 0) {
+                        this.FAC_isDivided = true;
+                        this.result += String(this.FAC_factor) + ", ";
+                        this.FAC_dividedNumber = this.FAC_dividedNumber / this.FAC_factor;
+                        //alert("Czynniki: " + this.result + " | Pozostałość: " + this.FAC_dividedNumber + "| i: " + this.FAC_factor);
+                    } else if (this.FAC_dividedNumber % this.FAC_factor !== 0) {
+                        this.FAC_factor++;
+                    }
+                }
+            }
+            this.screen_INFO.textContent = "FAC | " + this.value + " | Wynik:";
+            this.FAC_isNotComma = true;
+            for (let i: number = 0; i < this.result.length; i++) {
+                if (this.result[i] === ",") {
+                    this.FAC_isNotComma = false;
+                }
+            }
+
+            // Ogarnij wyznaczanie liczby pierwszej!
+            if (this.FAC_isNotComma === false) {
+                this.result = this.result.slice(0, this.result.length - 2);
+            } else if (this.FAC_isNotComma === true) {
+                this.result = "Liczba pierwsza";
+            }
+            this.screen_VALUE.textContent = this.result;
+        }
+    },
+    operation_DEL(): void {
+        // Jeżeli ostatni indeks ma wartość " " (" " za znakiem [,]), skasuj dwa miejsca, w przeciwnym razie 1:
+        if (this.value[this.value.length - 1] === " ") {
+            this.value = this.screen_VALUE.textContent.slice(0, (this.screen_VALUE.textContent.length - 2));
+        } else {
+            this.value = this.screen_VALUE.textContent.slice(0, (this.screen_VALUE.textContent.length - 1));
+        }
+        if (this.value.length === 0) {
+            this.value = "0";
+        }
+        this.screen_VALUE.textContent = this.value;
+    },
+    operation_AC(): void {
+        this.value = "0";
+        this.screen_VALUE.textContent = this.value;
+    },
+    operation_Comma(): void {
+        this.value += ", ";
+        this.screen_VALUE.textContent = this.value;
+    },
+    operation_Number(numKey: string): void {
+        // Jeżeli długośc wyrazu wynosi 1 i ma on wartośc 0, skasuj 0, a wstaw cyfrę, w przeciwnym razie dodaj cyfrę do wyrażenia:
+        if (this.value.length === 1 && this.value === "0") {
+            this.value = numKey;
+        } else if (this.value.length > 1 || this.value !== "0") {
+            this.value += numKey;
+        }
+        this.screen_VALUE.textContent = this.value;
+    },
 }
-Layout_ALL.setAppBody_Height_AEL();
-
-
-// Błąd! Przy NWW nie można używać [,]
-// Błąd! Przy FAC nie można używać [,]
-// Błąd! Muszisz podać minimum 2 liczby   // NWD i NWW
-// [liczba] | NWD | Wynik:   // [liczba]
-// [liczba] | NWW | Wynik:   // [liczba]
-// [liczba] | FAC | Wynik:   // [liczby], ["liczba pierwsza"]
-// Błąd! Wprowadź poprawne dane
-
-
-
-// Ustawienie pozycji scrollbaru na właściwym miejscu (ekran kalkulatora)
-window.addEventListener("load", () => {
-    const container: NodeListOf<Element> = document.querySelectorAll('.ct-refactoring > .screen > .screen-position > .screen-hanger > .info, .ct-refactoring > .screen > .screen-position > .screen-hanger > .value');
-    for (let i: number = 0; i < container.length; i++) {
-        container[i].scrollLeft = container[i].scrollWidth;
-    }
-}, false);
+Calculator_NWD_NWW_Faction_FUNCTIONS.setButtons_AEL();
